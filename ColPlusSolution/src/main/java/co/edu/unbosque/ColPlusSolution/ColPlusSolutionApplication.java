c18f40059a11648ea83f6fbe04445e431acc37a8
Cristhian Diaz <cristhiancamilodiazromero@gmail.com>
2024-10-29 20:36:14 -0500

    FIRST UPLOAD

---
+++ ColPlusSolution/src/main/java/co/edu/unbosque/ColPlusSolution/ColPlusSolutionApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.ColPlusSolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColPlusSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColPlusSolutionApplication.class, args);
+	}
+
+}
