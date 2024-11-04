ec2d5892fc7fa72ad40c98dd20076f17761dc374
MakingMeans <117835098+MakingMeans@users.noreply.github.com>
2024-11-03 21:50:35 -0500

    Java classes for Model
    User, Book, Movie, LeaveRecord, LoginRecord, Payroll

---
+++ ColPlusSolution/src/main/java/co/edu/unbosque/ColPlusSolution/Model/Movie.java
@@ -0,0 +1,70 @@
+package co.edu.unbosque.ColPlusSolution.Model;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "movie")
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "movie_id")
+    private Integer movieId;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "year")
+    private Integer year;
+
+    @ElementCollection
+    @Column(name = "genre")
+    private List<String> genre;
+
+    public Movie() {
+    	// TODO Auto-generated constructor stub
+    }
+
+	public Movie(Integer movieId, String title, Integer year, List<String> genre) {
+		super();
+		this.movieId = movieId;
+		this.title = title;
+		this.year = year;
+		this.genre = genre;
+	}
+
+	public Integer getMovieId() {
+		return movieId;
+	}
+
+	public void setMovieId(Integer movieId) {
+		this.movieId = movieId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+
+	public List<String> getGenre() {
+		return genre;
+	}
+
+	public void setGenre(List<String> genre) {
+		this.genre = genre;
+	}
+
+    
+}
