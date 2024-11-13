7614400ef147a9c8a00bd27aac7fca562f9a9eed
Cristhian Diaz <cristhiancamilodiazromero@gmail.com>
2024-11-13 00:10:59 -0500

    Update LoginRecord.java

--- ColPlusSolution/src/main/java/co/edu/unbosque/ColPlusSolution/model/LoginRecord.java
+++ ColPlusSolution/src/main/java/co/edu/unbosque/ColPlusSolution/model/LoginRecord.java
@@ -1,36 +1,56 @@
 package co.edu.unbosque.ColPlusSolution.model;
 
 import jakarta.persistence.*;
+
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Entity
 @Table(name = "login_record")
 public class LoginRecord {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "record_id")
-    private Integer recordId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "record_id")
+	private Integer recordId;
 
-    @Column(name = "date")
-    private Date date;
+	@Column(name = "date")
+	private Date date;
 
-    @Column(name = "time")
-    private Date time;
+	@Column(name = "time")
+	private Date time;
 
-    @Column(name = "user")
-    private String user;
+	@Column(name = "user")
+	private String user;
 
-    public LoginRecord() {
-    	// TODO Auto-generated constructor stub
-    }
+	public LoginRecord() {
+		// TODO Auto-generated constructor stub
+	}
 
-    public LoginRecord(Integer recordId, Date date, Date time, String user) {
-        this.recordId = recordId;
-        this.date = date;
-        this.time = time;
-        this.user = user;
-    }
+	public LoginRecord(Integer recordId, Date date, Date time, String user) {
+		this.recordId = recordId;
+		this.date = justDate(date);
+		this.time = justTime(time);
+		this.user = user;
+	}
+
+	private Date justDate(Date date) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		try {
+			return dateFormat.parse(dateFormat.format(date));
+		} catch (Exception e) {
+			return date;
+		}
+	}
+
+	private Date justTime(Date time) {
+		SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
+		try {
+			return timeFormat.parse(timeFormat.format(time));
+		} catch (Exception e) {
+			return time;
+		}
+	}
 
 	public Integer getRecordId() {
 		return recordId;
@@ -63,5 +83,5 @@ public class LoginRecord {
 	public void setUser(String user) {
 		this.user = user;
 	}
-	
+
 }
